2e336c0474996c967485f9313fc0a6471c87d9d2
Guilherme Costa <Guilherme Costa>
2017-02-13 19:02:21 -0500

    changes to main screen

--- WebSite1/www/scripts/my-app.js
+++ WebSite1/www/scripts/my-app.js
@@ -44,3 +44,13 @@ function createContentPage() {
     );
 	return;
 }
+
+var myApp = new Framework7(); 
+
+var mySwiper = myApp.swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  paginationHide: false,
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+}); 
