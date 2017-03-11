c27b2c759f3760c2a27e311a6245d5e01fd16ffd
Arthur Hilgendorf <Arthur Hilgendorf>
2017-03-10 21:58:52 -0500

    updated video-related functions

--- WebSite1/www/main_activity/createDrowpdown.js
+++
@@ -1,7 +0,0 @@
-<script>
-		var myArray = new Array("upOnHigh.MOV", "highGui.MOV");
-		for(i=0; i<myArray.length; i++) 
-		{  
-			document.write('<option value="' + myArray[i] +'">' + myArray[i] + '</option>');
-		}	
-</script>
--- WebSite1/www/main_activity/mainScript.js
+++ WebSite1/www/main_activity/mainScript.js
@@ -1,10 +1,14 @@
-﻿myApp.onPageInit('main', function (page) {
+﻿myApp.onPageInit('main', function (page) 
+{
     console.log('Main initialized');
 
-    $("#toggle").click(function () {
-        if ($('#panel').css('display') == 'block') {
+    $("#toggle").click(function () 
+	{
+        if ($('#panel').css('display') == 'block') 
+		{
             var height = '-=' + $('#panel').height();
-        } else {
+        } else 
+		{
             var height = '+=' + $('#panel').height();
         }
         $("#panel").slideToggle("slow");
