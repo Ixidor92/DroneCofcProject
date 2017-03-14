27eb9c74dd774c51871f079733c85b0c8864879b
Guilherme Costa <Guilherme Costa>
2017-03-13 23:50:37 -0400

    changes to GUI

--- WebSite1/www/main_activity/functions.js
+++ WebSite1/www/main_activity/functions.js
@@ -6,6 +6,26 @@ function changeVideo(element, choice)
 			
 	element.load();
 	element.play();
+	//<link rel="stylesheet" href="/WebSite1/www/main_activity/mainStyle.css">
+	//jQuery.get('http://localhost:61012/DJI_0013.srt', function (data) {
+	//    var lines = data.split("\n");
+
+	//    console.log(lines);
+
+
+
+
+    //    })
+    ///WebSite1/www/index_activity/dronebeach.jpg
+	$.ajax({
+	    url: "/WebSite1/www/index_activity/DJI_0013.srt", success: function (file_content) {
+	        console.log(file_content);
+            console.log("SUCCESS")
+	    }
+	});
+
+
+
 }
 				
 					
@@ -22,8 +42,8 @@ function addSourceToVideo(element, src)
 
 function createDropdown(element)
 {
-	var myArray = new Array("upOnHigh.MOV", "highGui.MOV");
-	for(i=0; i<myArray.length; i++) 
+    var myArray = new Array("exampleOne.mp4", "exampleTwo.mp4", "exampleThree.mp4", "exampleFour.mp4");
+    for (i = 0; i < myArray.length; i++)
 	{	  
 		var op = document.createElement('option');
 		var t = document.createTextNode(myArray[i]);
--- WebSite1/www/main_activity/mainScript.js
+++ WebSite1/www/main_activity/mainScript.js
@@ -9,19 +9,40 @@
             var height = '-=' + $('#panel').height();
             $('#panel').css('visibility', 'hidden');
 
-
         } else 
         {
             $('#lower').css('width', '100%');
-            $('#panel').css('visibility', 'show');
+            $('#panel').css('visibility', 'visible');
 
             var height = '+=' + $('#panel').height();
 
         }
         $("#panel").slideToggle("slow");
-
     });
-	
+
+
+
+
+createDropdown(videos);
+
+setInterval(function () {
+     vid = document.getElementById("beachVid");
+
+        getCurTime();
+    }, 1000);
+
+
 });
 
 
+
+function getCurTime() {
+    $("div#dronInfo").text(Math.round(vid.currentTime));
+
+
+   // alert(vid.currentTime);
+}
+
+function setCurTime() {
+    vid.currentTime = 5;
+}
