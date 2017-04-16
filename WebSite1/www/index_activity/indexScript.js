d8a0309dae3dd3978e8b27b0da1055beef0b99a9
Arthur Hilgendorf <arthurhilgendorf@Arthur-Hilgendorfs-MacBook-Pro-2.local>
2017-04-16 13:38:30 -0400

    canvas replaced Video

--- WebSite1/www/index_activity/indexScript.js
+++
@@ -1,7 +0,0 @@
-﻿var mySwiper = myApp.swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    paginationHide: false,
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-});
