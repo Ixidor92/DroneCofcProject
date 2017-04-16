a1c49d3a0acdabbc48a74715c570c1b4677b80d9
Arthur Hilgendorf <arthurhilgendorf@Arthur-Hilgendorfs-MacBook-Pro-2.local>
2017-04-16 13:41:37 -0400

    fixed botched push.  Replaced video with canvas

---
+++ WebSite1/www/index_activity/indexScript.js
@@ -0,0 +1,7 @@
+﻿var mySwiper = myApp.swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationHide: false,
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+});
---
+++ WebSite1/www/scripts/my-app.js
@@ -0,0 +1,15 @@
+﻿// Initialize your app
+var myApp = new Framework7({
+    template7Pages: true // enable Template7 rendering for Ajax and Dynamic pages
+});
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+});
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+
+
