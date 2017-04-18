c6a3c8af40861510d10c5ec2f57ed7218039817b
Guilherme Costa <Guilherme Costa>
2017-04-17 23:46:27 -0400

    new changes again

--- WebSite1/www/twoView_activity/twoViewScript.js
+++ WebSite1/www/twoView_activity/twoViewScript.js
@@ -93,7 +93,7 @@ function addLeftSource(element, src) {
     var source = document.createElement('source');
 
     source.id = "leftSource";
-    source.src = "../main_activity/follyVideos/" + src;
+    source.src = "../Beaches/FollyBeach/" + src;
     source.type = "video/mp4";
 
     element.appendChild(source);
@@ -103,7 +103,7 @@ function addRightSource(element, src) {
     var source = document.createElement('source');
 
     source.id = "rightSource";
-    source.src = "../main_activity/follyVideos/" + src;
+    source.src = "../Beaches/FollyBeach/" + src;
     source.type = "video/mp4";
 
     element.appendChild(source);
