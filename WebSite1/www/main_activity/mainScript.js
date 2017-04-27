98f33c44f7ba978bb02f28b2aadccd62f05cf5d9
Guilherme Costa <Guilherme Costa>
2017-04-26 20:24:41 -0400

    Final push of the project

--- WebSite1/www/main_activity/mainScript.js
+++ WebSite1/www/main_activity/mainScript.js
@@ -1,4 +1,7 @@
-﻿var map;
+﻿var canvas, context, tool;
+
+
+var map;
 var vid;
 var beachName ='';
 //srt parsing files
@@ -33,8 +36,24 @@ var kmlTemp = ['https://dl.dropboxusercontent.com/s/mklumc400rhofjn/charleston_P
 myApp.onPageInit('main', function (page)
 {
     //pixelSearch();
+    init();
+
+
+
+
 });
 
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+}
+
+function penColor(color) {
+    context.strokeStyle = color;
+
+
+
+}
 
 
 function initMap() {
@@ -127,7 +146,9 @@ function changeVideo(element, choice) {
     var indexOfChoice = y[x].index;
     var srtURL = "http://153.9.205.25/~aecom/WebSite1/www/getSrt.php?srtFile=" + srtSrcArray[indexOfChoice-1] +'&beach='+beachName;
 
-    console.log(srtURL)
+    console.log(srtSrcArray);
+    console.log(videoSrcArray)
+
 
     $.ajax({
         url: srtURL,
@@ -352,7 +373,7 @@ function pixelSearch() {
     //document.addEventListener('DOMContentLoaded', function () {
         var v = document.getElementById('beachVid');
         var canvas = document.getElementById('c');
-        var context = canvas.getContext('2d');
+       // var context = canvas.getContext('2d');
 
         var cw = Math.floor(canvas.clientWidth);
         var ch = Math.floor(canvas.clientHeight);
@@ -406,6 +427,9 @@ function loadBeach(beach) {
                         break;
                     case 'MOV':
                         videoSrcArray.push(filename);
+                        var res = filename.split(".");
+                        res = res[0] + ".SRT";
+                        srtSrcArray.push(res);
                         break;
                         
 
@@ -450,4 +474,107 @@ function loadBeach(beach) {
 
 
     
+}
+
+
+
+function init() {
+    // Find the canvas element.
+    canvas = document.getElementById('imageView');
+    fitToContainer(canvas);
+
+    if (!canvas) {
+        alert('Error: I cannot find the canvas element!');
+        return;
+    }
+
+    if (!canvas.getContext) {
+        alert('Error: no canvas.getContext!');
+        return;
+    }
+
+    // Get the 2D canvas context.
+    context = canvas.getContext('2d');
+    if (!context) {
+        alert('Error: failed to getContext!');
+        return;
+    }
+
+    // Pencil tool instance.
+    tool = new tool_pencil();
+
+    // Attach the mousedown, mousemove and mouseup event listeners.
+    canvas.addEventListener('mousedown', ev_canvas, false);
+    canvas.addEventListener('mousemove', ev_canvas, false);
+    canvas.addEventListener('mouseup', ev_canvas, false);
+}
+
+// This painting tool works like a drawing pencil which tracks the mouse 
+// movements.
+function tool_pencil() {
+    var tool = this;
+    this.started = false;
+
+    // This is called when you start holding down the mouse button.
+    // This starts the pencil drawing.
+    this.mousedown = function (ev) {
+        //context.strokeStyle = '#ff0000';
+
+        context.beginPath();
+        context.moveTo(ev._x, ev._y);
+        tool.started = true;
+    };
+
+    // This function is called every time you move the mouse. Obviously, it only 
+    // draws if the tool.started state is set to true (when you are holding down 
+    // the mouse button).
+    this.mousemove = function (ev) {
+        if (tool.started) {
+            context.lineTo(ev._x, ev._y);
+            //context.lineWidth = 15;
+
+            //context.strokeStyle = '#ff0000';
+            //context.lineWidth = 15;
+
+
+            context.stroke();
+        }
+    };
+
+    // This is called when you release the mouse button.
+    this.mouseup = function (ev) {
+        if (tool.started) {
+            convtext.closePath();
+            tool.mousemove(ev);
+            tool.started = false;
+        }
+    };
+}
+
+// The general-purpose event handler. This function just determines the mouse 
+// position relative to the canvas element.
+function ev_canvas(ev) {
+    if (ev.layerX || ev.layerX == 0) { // Firefox
+        ev._x = ev.layerX;
+        ev._y = ev.layerY;
+    } else if (ev.offsetX || ev.offsetX == 0) { // Opera
+        ev._x = ev.offsetX;
+        ev._y = ev.offsetY;
+    }
+
+    // Call the event handler of the tool.
+    var func = tool[ev.type];
+    if (func) {
+        func(ev);
+    }
+}
+
+
+function fitToContainer(canvas) {
+    // Make it visually fill the positioned parent
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    // ...then set the internal size to match
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
 }
--- WebSite1/www/twoView_activity/testScript.js
+++ WebSite1/www/twoView_activity/testScript.js
@@ -1,33 +1,106 @@
+var canvas, context, tool;
 
-myApp.onPageInit('textpage', function (page) {
+myApp.onPageInit('testpage', function (page) {
 
-    var el = document.getElementById('c2');
-    var ctx = el.getContext('2d');
-    var isDrawing;
 
-   // console.log("IM HERE")
+
+
+            console.log('here')
+
+            
+
+            init();
+
+
+    // vim:set spell spl=en fo=wan1croql tw=80 ts=2 sw=2 sts=2 sta et ai cin fenc=utf-8 ff=unix:
+
+
+
+        
 
 
 
     
 
+
 });
 
-el.onmousedown = function (e) {
-    console.log("HERE")
-    isDrawing = true;
-    ctx.moveTo(e.clientX, e.clientY);
-};
-el.onmousemove = function (e) {
-    console.log("HERE")
-
-    if (isDrawing) {
-        ctx.lineTo(e.clientX, e.clientY);
-        ctx.stroke();
+function init() {
+    // Find the canvas element.
+    canvas = document.getElementById('imageView');
+    if (!canvas) {
+        alert('Error: I cannot find the canvas element!');
+        return;
     }
-};
-el.onmouseup = function () {
-    console.log("HERE")
 
-    isDrawing = false;
-};
+    if (!canvas.getContext) {
+        alert('Error: no canvas.getContext!');
+        return;
+    }
+
+    // Get the 2D canvas context.
+    context = canvas.getContext('2d');
+    if (!context) {
+        alert('Error: failed to getContext!');
+        return;
+    }
+
+    // Pencil tool instance.
+    tool = new tool_pencil();
+
+    // Attach the mousedown, mousemove and mouseup event listeners.
+    canvas.addEventListener('mousedown', ev_canvas, false);
+    canvas.addEventListener('mousemove', ev_canvas, false);
+    canvas.addEventListener('mouseup', ev_canvas, false);
+}
+
+// This painting tool works like a drawing pencil which tracks the mouse 
+// movements.
+function tool_pencil() {
+    var tool = this;
+    this.started = false;
+
+    // This is called when you start holding down the mouse button.
+    // This starts the pencil drawing.
+    this.mousedown = function (ev) {
+        context.beginPath();
+        context.moveTo(ev._x, ev._y);
+        tool.started = true;
+    };
+
+    // This function is called every time you move the mouse. Obviously, it only 
+    // draws if the tool.started state is set to true (when you are holding down 
+    // the mouse button).
+    this.mousemove = function (ev) {
+        if (tool.started) {
+            context.lineTo(ev._x, ev._y);
+            context.stroke();
+        }
+    };
+
+    // This is called when you release the mouse button.
+    this.mouseup = function (ev) {
+        if (tool.started) {
+            tool.mousemove(ev);
+            tool.started = false;
+        }
+    };
+}
+
+// The general-purpose event handler. This function just determines the mouse 
+// position relative to the canvas element.
+function ev_canvas(ev) {
+    if (ev.layerX || ev.layerX == 0) { // Firefox
+        ev._x = ev.layerX;
+        ev._y = ev.layerY;
+    } else if (ev.offsetX || ev.offsetX == 0) { // Opera
+        ev._x = ev.offsetX;
+        ev._y = ev.offsetY;
+    }
+
+    // Call the event handler of the tool.
+    var func = tool[ev.type];
+    if (func) {
+        func(ev);
+    }
+}
